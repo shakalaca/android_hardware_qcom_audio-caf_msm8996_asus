b820fc9832172ff680f60bfb29c6c905bae88275
Shaka Huang <shakalaca@gmail.com>
2017-05-14 17:49:19 +0800

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,9 @@
+MY_LOCAL_PATH := $(call my-dir)
+
+include $(MY_LOCAL_PATH)/hal/Android.mk
+include $(MY_LOCAL_PATH)/voice_processing/Android.mk
+include $(MY_LOCAL_PATH)/mm-audio/Android.mk
+include $(MY_LOCAL_PATH)/policy_hal/Android.mk
+include $(MY_LOCAL_PATH)/visualizer/Android.mk
+include $(MY_LOCAL_PATH)/audiod/Android.mk
+include $(MY_LOCAL_PATH)/post_proc/Android.mk
